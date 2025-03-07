b482fe0e54d7c61cc01a1e35a4d23dee00810e10
Ericlemes <eric_efl@hotmail.com>
2025-03-07 15:42:52 -0300

    Clean up Terraform configuration by removing unnecessary variables

--- variables.tf
+++ variables.tf
@@ -1,8 +1,4 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
+
 
 variable "environment" {
   description = "Environment name (staging or production)"
